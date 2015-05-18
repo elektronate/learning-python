95419f308d36af092857e0a1cbd5ece097a0261c
rerere <elektronate@users.noreply.github.com>
2015-05-18 20:30:18 +0200

    Create ex03.py
    "Numbers and Math"

---
+++ ex03.py
@@ -0,0 +1,27 @@
+# "Numbers and Math"
+# +  -  /  *  %  <  >  <=  >=
+# a = puts out string
+# b = does the math, puts out integer OR True or False
+
+print "I will now count my chickens:" # a
+
+print "Hens", 25.0 + 30.0 / 6.0 # a,b
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0 # a,b
+
+print "Now I will count my eggs:" # a
+
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0 # b
+
+print "Is it true that 3 + 2 < 5 - 7?" # a
+
+print 3 + 2 < 5 - 7 # b
+
+print "What is 3 + 2?", 3 + 2 # a,b
+print "What is 5 - 7?", 5 - 7 # a,b
+
+print "Oh, that's why it's False." # a
+print "How about some more." #a
+
+print "Is it greater?", 5 > -2 # b
+print "Is it greater or equal?", 5 >= -2 # a,b
+print "Is it less or equal?", 5 <= -2 # a,b
