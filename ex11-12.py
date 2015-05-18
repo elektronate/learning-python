152e6e8de1d408f88c62ec3b8d6d98b6cf3dad47
rerere <elektronate@users.noreply.github.com>
2015-05-19 00:18:11 +0200

    Update and rename ex11.py to ex11-12.py

--- ex11-12.py
+++ ex11-12.py
@@ -6,12 +6,9 @@
 name = raw_input("Hi! What's your name? ")
 print "So glad I found you, %s! I wanna know everything about you." % name
 
-print "How old are you?",
-age = int(raw_input())
-print "Where do you live?",
-location = raw_input()
-print "What's your favorite dish?",
-dish = raw_input()
+age = int(raw_input("How old are you?"))
+location = raw_input("Where do you live?")
+dish = raw_input("What's your favorite dish?")
 
 print "So, you're %r, huh?" % age
 print "I love %r! We should have some %r when I come visit." % (
