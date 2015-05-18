ff739937c2744980f62d4967dfeb0d0de5ea1edf
rerere <elektronate@users.noreply.github.com>
2015-05-18 20:38:34 +0200

    Create ex04.py

---
+++ ex04.py
@@ -0,0 +1,21 @@
+#"Variables and Names"
+# note underscore character
+
+# defining variables (number of cars, seats, drivers, passengers)
+cars = 100
+space_in_a_car = 4
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+# placing values, doing the math "inside" of strings
+# puts out integers surrounded by strings
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
