6c891317c89f3b844e5e1d884bb2cf90a880289e
rerere <elektronate@users.noreply.github.com>
2015-05-18 20:05:54 +0200

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,6 +1,9 @@
 # "print" tells the computer to give an output. 
 # Mind the quotation marks (single and double). 
-# Octothorpe/hash/mesh/pound allows you to write comments inside the script, that will be ignored by the computer.
+# Octothorpe/hash/mesh/pound allows you to write comments 
+# inside the script, that will be ignored by Python.
+# Can also be used to "disable" or comment out a piece of code:
+# print "This won't run."
 
 print "Hello GitHub!"
 print "Here I am, writing my first piece of code."
