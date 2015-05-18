56a5d650983f38ed09c7055f4afda8dc17c744a0
rerere <elektronate@users.noreply.github.com>
2015-05-18 23:15:45 +0200

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,10 +1,13 @@
 # "What was That?"
 # Escape Sequences
 # \ (backslash) encodes difficult-to-type characters into a string
-# \\  \'  \"  \a  \b  \f  \n  \N{name}  \r  \t
+# \\  \'  \"  \a  \b  \n  \N{name}  \r  \t
+# \f (form-feed) moves printing position to next logical page
+# \v (vertical-tab) moves printing position to next vt position
 
 tabby_cat = "\tI'm tabbed in."
-persian_cat = "I'm split\non a line."
+persian_cat = "\vI'm split\non a line."
+claustro_cat = "\fI need air."
 backslash_cat = "I'm \\ a \\ cat."
 
 fat_cat = """
@@ -16,7 +19,9 @@ I'll do a list:
 
 print tabby_cat
 print persian_cat
+print claustro_cat
 print backslash_cat
 print fat_cat
+
 print "The cat says: \"%s\"" % tabby_cat
 print "The cat says: \"%r\"" % tabby_cat
