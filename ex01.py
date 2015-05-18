6c11ba803cdcdc14b2303da351e8f8b355d3cbd8
rerere <elektronate@users.noreply.github.com>
2015-05-18 19:54:32 +0200

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,9 @@
+# "print" tells the computer to give an output. 
+# Mind the quotation marks (single and double). 
+# Octothorpe/hash/mesh/pound allows you to write comments inside the script, that will be ignored by the computer.
+
+print "Hello GitHub!"
+print "Here I am, writing my first piece of code."
+print 'Is this a "program" at all?'
+print "Not sure. At least it's printing."
+print 'Just keep going!'
