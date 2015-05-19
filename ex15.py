7b4997d61230ac7173661b1aead8d34af9ae7c63
rerere <elektronate@users.noreply.github.com>
2015-05-20 00:50:53 +0200

    Update ex15.py

--- ex15.py
+++ ex15.py
@@ -12,11 +12,11 @@ txt = open(filename) # gets file
 print "Here's your file %r:" % filename # prints string w/filename
 print txt.read() # displays file
 
-print txt.close() # closes file
+txt.close() # closes file
 
 txt = open(filename) # opens file again
 
 file_again = raw_input("Type the filename again: ")
 print txt.read()
 
-print txt.close()
+txt.close()
